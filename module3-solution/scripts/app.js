ed3eb2571466fba266fa33a347a3ae33e17942b4
vahndi.minah <vahndi.minah@arup.com>
2016-10-09 17:03:21 -0400

    modified templateUrl

--- module3-solution/scripts/app.js
+++ module3-solution/scripts/app.js
@@ -15,7 +15,7 @@
                 items: "<",
                 onRemove: '&'
             },
-            templateUrl: '../directives/foundItems.html',
+            templateUrl: '../module3-solution/directives/foundItems.html',
             controller: NarrowItDownController,
             controllerAs: 'narrowIt',
             bindToController: true
